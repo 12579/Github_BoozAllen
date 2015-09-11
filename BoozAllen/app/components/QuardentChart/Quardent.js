fdda8a09ab2fb5fb2458923537e1b007b781b76e
12579 <shalini.garg@compunnel.in>
2015-09-11 19:26:32 +0530

    Extra Files Deleted

--- BoozAllen/app/components/QuardentChart/Quardent.js
+++
@@ -1,9 +0,0 @@
-﻿var app = angular.module("nvd3TestApp", []).controller('ExampleCtrl1',
-    function ExampleCtrl1($scope, $http) {
-        $http.get("../../../sampleJson/kpi.json").then(function (res) {
-         
-            $scope.exampleData = res.data;
-
-        });
-  }
-);
--- BoozAllen/app/components/riskDNA/riskDNA1.js
+++
@@ -1,8 +0,0 @@
-﻿var app = angular.module('dnaApp', ['dnaDirectives']).controller('dnaCtrl',
-    function dnaCtrl($scope, $http) {
-        $http.get("../../../sampleJson/Risk.json").then(function (res) {
-            $scope.dnaData = res.data;
-        });
-        
-    }
-);
